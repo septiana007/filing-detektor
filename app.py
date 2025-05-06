922dab13a43f17daa4d1f9a6bdeab27f398b2814
septiana007 <septianalaoli1@gmail.com>
2025-05-06 10:27:04 +0700

    hvtguy

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st 
 from textblob import TextBlob 
 
-st,title("Simple Sentiment Analysis")
+st.title("Simple Sentiment Analysis")
 
 text = st.text_area("Enter a sentence or review")
 if st.button("Analyze"):
